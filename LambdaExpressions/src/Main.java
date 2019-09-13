7484c8b8fc9829f324b40221f192db30ad0ee9d0
Pasha V <yourwaof@gmail.com>
2019-09-13 11:58:30 +0300

    Correction of Lambda expression written in Main

--- LambdaExpressions/src/Main.java
+++ LambdaExpressions/src/Main.java
@@ -15,9 +15,10 @@ public class Main
         ArrayList<Employee> staff = loadStaffFromFile();
 
         Collections.sort(staff, (o1, o2) ->
-                {if (o1.getSalary() == o2.getSalary()) {return o1.getName().compareTo(o2.getName());}
+                {
+                    if (o1.getSalary() - o2.getSalary() == 0) {return o1.getName().compareTo(o2.getName());}
                 else
-                    return o1.getSalary().compareTo(o2.getSalary());}
+                    return o1.getSalary() - o2.getSalary(); }
         );
 
         for (Employee employee : staff){
