9c3499da1def24fc7df584a5d727908ed406a181
Pasha V <yourwaof@gmail.com>
2019-09-13 11:29:04 +0300

    Lambda expression written in Main

--- LambdaExpressions/src/Main.java
+++ LambdaExpressions/src/Main.java
@@ -14,6 +14,15 @@ public class Main
     {
         ArrayList<Employee> staff = loadStaffFromFile();
 
+        Collections.sort(staff, (o1, o2) ->
+                {if (o1.getSalary() == o2.getSalary()) {return o1.getName().compareTo(o2.getName());}
+                else
+                    return o1.getSalary().compareTo(o2.getSalary());}
+        );
+
+        for (Employee employee : staff){
+            System.out.println(employee);
+        }
 
     }
 
